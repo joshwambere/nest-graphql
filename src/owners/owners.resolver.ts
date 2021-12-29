201766d3baf2019f5f831e465284fd7b0e0398dc
josh <joshwambere@gmail.com>
2021-12-30 00:29:07 +0200

    added auth0 support

--- src/owners/owners.resolver.ts
+++ src/owners/owners.resolver.ts
@@ -3,11 +3,14 @@ import { OwnersService } from './owners.service';
 import { Owner } from './entities/owner.entity';
 import { CreateOwnerInput } from './dto/create-owner.input';
 import { UpdateOwnerInput } from './dto/update-owner.input';
+import { UseGuards } from '@nestjs/common';
+import { AuthorizationGuard } from 'src/authorization.guard';
 
 @Resolver(() => Owner)
 export class OwnersResolver {
   constructor(private readonly ownersService: OwnersService) {}
 
+  @UseGuards(AuthorizationGuard)
   @Mutation(() => Owner)
   createOwner(@Args('createOwnerInput') createOwnerInput: CreateOwnerInput) {
     return this.ownersService.create(createOwnerInput);
